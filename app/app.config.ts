a62f1389ee4293ce92de2bde5d038bc02695235e
tom harrison <thomas.harrison@conrad.cz>
2026-03-07 23:35:17 +0100

    add meta

--- app/app.config.ts
+++ app/app.config.ts
@@ -8,5 +8,14 @@ export default defineAppConfig({
   socials: {
     instagram: 'https://instagram.com/hotrecipes',
     twitter: 'https://twitter.com/hotrecipes'
+  },
+  meta: {
+    title: 'Nuxt 4 Boilerplate | Nuxt 4 starter 🚀',
+    description: 'Nuxt Starter, Nuxt UI, Nuxt Content, Nuxt Image, Nuxt Fonts, Vueuse, Pinia, TailwindCSS, i18n, and more.',
+    keywords: 'nuxt, vite, starter',
+    image: '/cover.png',
+    author: 'Thinh Le',
+    publisher: 'Thinh Le',
+    canonical: 'https://github.com/heydayle/nuxt-boilerplate'
   }
 })
